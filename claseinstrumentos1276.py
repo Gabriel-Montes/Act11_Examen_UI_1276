e69366d5cb934fe1bd972d9414eee4fcda39f979
Gabriel-Montes <a.22308051281276@cbtis128.edu.mx>
2024-09-26 11:55:10 -0600

    Add files via upload

---
+++ claseinstrumentos1276.py
@@ -0,0 +1,58 @@
+print("EXAMEN ALUMNO:1276")
+Id_Ins1276=0
+mante1276=0
+marca1276=""
+estado1276=""
+lugar1276=""
+tamaño1276=0.0
+vendido1276=False
+class instrumentos1276:
+    def mos_datos1276(self):
+        print(Id_Ins1276,mante1276,marca1276,estado1276,lugar1276,tamaño1276,vendido1276)
+    def list_instru1276(self):
+        ins1276=['piano','Clarinete','Trompeta','bateria','guitarra','flauta','fagot']
+        print(ins1276)
+        for x in ins1276:
+            print(x)
+    def tup_cliente1276(self):
+        marca1276=('pedro','marco','eva','luis','adan','ana','romina')
+        print(marca1276)
+        for x in marca1276:
+            print(x)
+    def dic_prove_cant1276(self):
+        prove1276={
+            'yamaha':5,
+            'holton':4,
+            'vandoren':9,
+            'besson':4,
+            'sabian':3,
+            'gibson':10,
+            'fender':8
+        }
+        print(prove1276)
+        for x,y in prove1276.items():
+            print(x,y)
+    def regis1276(self):
+        print('todo se ha registrado correctamente\n')
+    def clear1276(self):
+        print('La base de datos se a borrado correctamente\n')
+    
+
+mos1276=instrumentos1276()
+
+mos1276.mos_datos1276()
+print()
+mos1276.list_instru1276()
+print()
+mos1276.tup_cliente1276()
+print()
+mos1276.dic_prove_cant1276()
+print()
+mos1276.regis1276()
+mos1276.clear1276()
+
+
+
+
+# python claseinstrumentos1276
+# 1276
